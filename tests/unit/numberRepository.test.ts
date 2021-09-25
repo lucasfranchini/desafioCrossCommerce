8c5af1761c9344910a1f597cef9bfb9f7ea97350
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-25 02:09:01 -0300

    feat: tests for numberRepository

---
+++ tests/unit/numberRepository.test.ts
@@ -0,0 +1,17 @@
+import * as numberRepository from "../../src/repositories/numberRepository";
+import axios from "axios";
+
+describe("getAll", () => {
+  it("it throws an error for emptyArray in first get", async () => {
+    jest.spyOn(axios, "get").mockImplementationOnce(async () => {return { data: { numbers: [] } };});
+    const result = () => numberRepository.getAll();
+    await expect(result).rejects.toThrow(Error);
+  });
+  it("it returns unordered Array", async () => {
+    jest.spyOn(axios, "get")
+      .mockImplementationOnce(async () => {return { data: { numbers: [1, 5, 2, 3] } };})
+      .mockImplementationOnce(async () => {return { data: { numbers: [] } };});
+    const result = await numberRepository.getAll();
+    expect(result).toEqual([1, 5, 2, 3]);
+  });
+});
--- tests/unit/orderArray.test.ts
+++ tests/unit/orderArray.test.ts
@@ -2,17 +2,20 @@ import orderArray from "../../src/helpers/orderArray";
 import * as helper from "../../src/helpers/partition";
 
 describe("orderArray", () => {
-  it("returns an ordered array for unordered arrays", () => {
+  it("calls partition helper until returned value equals to left/right values", () => {
     const array = [1, 5, 7, 9, 8, 10, 3, 158, 0.1];
-    const orderedArray = [0.1, 1, 3, 5, 7, 8, 9, 10, 158];
-    orderArray(array);
-    expect(array).toEqual(orderedArray);
+    const mockedPartition = jest.spyOn(helper, "default")
+      .mockImplementationOnce(() => {return 9;})
+      .mockImplementationOnce(() => {return 8;})
+      .mockImplementationOnce(() => {return 10;});
+    orderArray(array, 7, 10);
+    expect(mockedPartition).toHaveBeenCalledTimes(3);
   });
-
   it("returns an ordered array for unordered arrays", () => {
-    const array = [1, 5, 7, 9, 8, 10, 3, 158, 0.1];
-    const mockedPartition = jest.spyOn(helper, "default").mockImplementationOnce(() => {return 9;}).mockImplementationOnce(() => {return 8;});
-    orderArray(array, 7, 9);
-    expect(mockedPartition).toHaveBeenCalledTimes(2);
+    let array = [1, 5, 7, 9, 8, 10, 3, 158, 0.1];
+    const orderedArray = [0.1, 1, 3, 5, 7, 8, 9, 10, 158];
+    jest.spyOn(helper, "default").mockImplementationOnce(() => {array=orderedArray; return 9;});
+    orderArray(array, 8, 9);
+    expect(array).toEqual(orderedArray);
   });
 });
