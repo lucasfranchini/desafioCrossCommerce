b50dba12d6c5c7406216395a2ced92594364d345
lucasfranchini <lucasfranchini.a@gmail.com>
2021-09-28 00:13:42 -0300

    feat: paralel requistions

--- src/repositories/numberRepository.ts
+++ src/repositories/numberRepository.ts
@@ -5,15 +5,24 @@ export async function getAll() {
   let isLastArray = false;
   let i = 1;
   const array: number[] = [];
-  while(!isLastArray) {
+  while(!isLastArray ) {
     try{
-      const newArray = await getPage(i);
-      if(newArray.length===0)isLastArray = true;
-      else array.push(...newArray);
-      i++;
+      const requisicoes: Promise<number[]>[] = [];
+      let j = i;
+      while(j<i+10) {
+        requisicoes.push(getPage(j));
+        j++;
+      }
+      const numbers: number[]= [];
+      for await (const req of requisicoes) {
+        numbers.push(...req);
+        if(req.length===0)isLastArray = true;
+      }
+      array.push(...numbers);
+      i = j;
     }
     catch(e) {
-      // eslint-disable-next-line no-console
+    // eslint-disable-next-line no-console
       console.log(e.response);
       if(e.response.data.error !=="Simulated internal error") {
         isLastArray =true;
@@ -24,7 +33,7 @@ export async function getAll() {
   return array;
 }
 
-export async function getPage(id: number) {
+export async function getPage(id: number): Promise<number[]> {
   const result = await axios.get(`http://challenge.dienekes.com.br/api/numbers?page=${id}`);
   return result.data.numbers;
 }
